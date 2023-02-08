9ec68609e4541bc0f606558abfa5b06d49f19d78
soci222g <jordi.gratacos@enti.cat>
2023-02-08 14:06:37 +0100

    imagen i movimento

---
+++ index.js
@@ -0,0 +1,51 @@
+const http = require("http");
+const fs = require("fs");
+
+
+
+function send_index (response)
+{
+	fs.readFile("index.html", function(err, data){
+		if (err){
+			console.error(err);
+			return;
+		}
+
+		response.writeHead(200, {"Content-Type":"text/html"});
+		response.write(data);
+
+		response.end();
+	});
+}
+
+function send_player (response)
+{
+	fs.readFile("player.png", function(err, data){
+		if (err){
+			console.error(err);
+			return;
+		}
+
+		response.writeHead(200, {"Content-Type":"image/png"});
+		response.write(data);
+
+		response.end();
+	});
+}
+
+
+http.createServer(function(request, response){
+	console.log(request.url);
+
+	let url = request.url.split("/");
+
+	switch (url[1]){
+		case "player.png":
+			send_player(response);
+
+			break;
+
+		default:
+			send_index(response);
+	}
+}).listen(6969);
